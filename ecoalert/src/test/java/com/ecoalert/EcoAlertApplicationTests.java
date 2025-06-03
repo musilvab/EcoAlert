bb430f5a0c42b7c7d5fbb1e768450c388edb5c9e
Murilo Barbosa <91965497+musilvab@users.noreply.github.com>
2025-06-03 11:44:10 -0300

    Escopo do projeto

---
+++ ecoalert/src/test/java/com/ecoalert/EcoAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
