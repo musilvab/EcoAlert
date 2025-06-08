d535268103612e68c3685ff58bcd3dc2af0f1670
Murilo Barbosa <91965497+musilvab@users.noreply.github.com>
2025-06-08 18:25:10 -0300

    CRUD Finalizado

---
+++ EcoAlert-main/src/main/java/com/ecoalert/EcoAlertApplication.java
@@ -0,0 +1,13 @@
+package com.ecoalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoAlertApplication.class, args);
+	}
+
+}
