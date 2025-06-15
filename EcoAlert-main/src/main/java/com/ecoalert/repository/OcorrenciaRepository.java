c36a435709d179cd2966540d9cc9ff3fbe3e4abc
Murilo Barbosa <91965497+musilvab@users.noreply.github.com>
2025-06-15 15:51:56 -0300

    Delete EcoAlert-main directory

--- EcoAlert-main/src/main/java/com/ecoalert/repository/OcorrenciaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecoalert.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecoalert.entity.Ocorrencia;
-
-public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Long> {
-    
-}
